764063b0a4e509411f386e2dfd3acd00174aa78d
양준헌 <97436830+greyhawk16@users.noreply.github.com>
2023-12-01 11:07:32 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import subprocess, os
 
 app=Flask(__name__)
 
+
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def home():
+    return render_template("home.html")
+
+
+@app.route("/moo")
+def moo():
+    inp = request.args.get("keyword")
+    cmd = f"echo {inp}"
+    try:
+        output = subprocess.check_output(['/bin/sh', '-c', cmd], timeout=5)
+        return render_template('moo.html', data=output.decode('utf-8'))
+    except subprocess.TimeoutExpired:
+        return render_template('moo.html', data='Timeout !')
+    except subprocess.CalledProcessError:
+        return render_template('moo.html', data=f'an error occurred while executing the command. -> {cmd}')
+
 
 if __name__=='__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
