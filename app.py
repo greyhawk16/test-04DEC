8a79eae78c1fc073de5454ae3825d023adbafc30
양준헌 <97436830+greyhawk16@users.noreply.github.com>
2023-12-04 08:46:26 +0900

    Update app.py
    GET, POST 요청 구분 가능

--- app.py
+++ app.py
@@ -1,25 +1,27 @@
+# distinguish between GET & POST requests
+
+
 from flask import Flask, render_template, request
-import subprocess, os
+import subprocess
+
 
 app=Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
+    if request.method == 'POST':
+        inp = request.form.get("keyword")
+        cmd = f"echo {inp}"
+        try:
+            output = subprocess.check_output(['/bin/sh', '-c', cmd], timeout=5)
+            return render_template('moo.html', data=output.decode('utf-8'))
+        except subprocess.TimeoutExpired:
+            return render_template('moo.html', data='Timeout !')
+        except subprocess.CalledProcessError:
+            return render_template('moo.html', data=f'an error occurred while executing the command. -> {cmd}')
+    
     return render_template("home.html")
-
-
-@app.route("/moo")
-def moo():
-    inp = request.args.get("keyword")
-    cmd = f"echo {inp}"
-    try:
-        output = subprocess.check_output(['/bin/sh', '-c', cmd], timeout=5)
-        return render_template('moo.html', data=output.decode('utf-8'))
-    except subprocess.TimeoutExpired:
-        return render_template('moo.html', data='Timeout !')
-    except subprocess.CalledProcessError:
-        return render_template('moo.html', data=f'an error occurred while executing the command. -> {cmd}')
 
 
 if __name__=='__main__':
