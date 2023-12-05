ef6abf301094a81acf37b4bc083e7939033aa844
양준헌 <97436830+greyhawk16@users.noreply.github.com>
2023-12-05 12:29:46 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,26 @@
-# distinguish between GET & POST requests
-
-
 from flask import Flask, render_template, request
-import subprocess
+import subprocess, os
 
 
 app=Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def home():
-    if request.method == 'POST':
-        inp = request.form.get("keyword")
-        cmd = f"echo {inp}"
-        try:
-            output = subprocess.check_output(['/bin/sh', '-c', cmd], timeout=5)
-            return render_template('moo.html', data=output.decode('utf-8'))
-        except subprocess.TimeoutExpired:
-            return render_template('moo.html', data='Timeout !')
-        except subprocess.CalledProcessError:
-            return render_template('moo.html', data=f'an error occurred while executing the command. -> {cmd}')
-    
     return render_template("home.html")
+
+
+@app.route("/reflect")
+def reflect():
+    inp = request.args.get("keyword")
+    cmd = f"echo {inp}"
+    try:
+        output = subprocess.check_output(['/bin/sh', '-c', cmd], timeout=5)
+        return render_template('reflect.html', data=output.decode('utf-8'))
+    except subprocess.TimeoutExpired:
+        return render_template('reflect.html', data='Timeout !')
+    except subprocess.CalledProcessError:
+        return render_template('reflect.html', data=f'an error occurred while executing the command. -> {cmd}')
 
 
 if __name__=='__main__':
